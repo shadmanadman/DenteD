a5118835bfeb1cfad96df68c9573b63ef2938671
shadmanadman <adman.shadman@gmail.com>
2025-11-25 22:14:12 +0330

    Adding deps

--- composeApp/src/commonMain/kotlin/org/shad/adman/jaw/generation/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.shad.adman.jaw.generation
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/iosMain/kotlin/org/shad/adman/jaw/generation/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package org.shad.adman.jaw.generation
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
