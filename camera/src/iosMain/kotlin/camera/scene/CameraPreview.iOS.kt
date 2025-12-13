d2f5cd63ef67aada3fc60e5d25b2910869244696
shadmanadman <adman.shadman@gmail.com>
2025-12-13 13:15:39 +0330

    impl camera preview

---
+++ camera/src/iosMain/kotlin/camera/scene/CameraPreview.iOS.kt
@@ -0,0 +1,60 @@
+package camera.scene
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.UIKitViewController
+import camera.builder.CameraControllerBuilder
+import camera.controller.CameraController
+import com.kashif.cameraK.builder.createIOSCameraControllerBuilder
+import platform.Foundation.NSNotificationCenter
+import platform.UIKit.UIDeviceOrientationDidChangeNotification
+
+/**
+ * iOS-specific implementation of [CameraPreview].
+ *
+ * @param modifier Modifier to be applied to the camera preview.
+ * @param cameraConfiguration Lambda to configure the [CameraControllerBuilder].
+ * @param onCameraControllerReady Callback invoked with the initialized [CameraController].
+ */
+@Composable
+actual fun ExpectCameraPreview(
+    modifier: Modifier,
+    cameraConfiguration: CameraControllerBuilder.() -> Unit,
+    onCameraControllerReady: (CameraController) -> Unit
+) {
+
+    val cameraController = remember {
+        createIOSCameraControllerBuilder()
+            .apply(cameraConfiguration)
+            .build()
+    }
+
+    LaunchedEffect(cameraController) {
+        onCameraControllerReady(cameraController)
+    }
+
+    DisposableEffect(Unit) {
+        val notificationCenter = NSNotificationCenter.defaultCenter
+        val observer = notificationCenter.addObserverForName(
+            UIDeviceOrientationDidChangeNotification,
+            null,
+            null
+        ) { _ ->
+            cameraController.getCameraPreviewLayer()?.connection?.videoOrientation =
+                cameraController.currentVideoOrientation()
+        }
+
+        onDispose {
+            notificationCenter.removeObserver(observer)
+        }
+    }
+
+    UIKitViewController(
+        factory = { cameraController },
+        modifier = modifier,
+    )
+}
